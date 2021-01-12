4ee7d1bb2acc680f7e2700c57d2d7d363cb8f4bc
Nebula4869 <44956069+Nebula4869@users.noreply.github.com>
2021-01-12 22:55:12 +0800

    Update main.cpp

--- Real-timePanoramicStitching/main.cpp
+++ Real-timePanoramicStitching/main.cpp
@@ -2,10 +2,11 @@
 #include "imageStitcher.h"
 
 
-void main()
+int main()
 {
 	const char *imagePaths[] = { "A.JPG", "B.JPG", "C.JPG", "D.JPG" };
 	stitchImage(imagePaths, 4);
 	const char cameraIDs[] = { 0, 1 };
 	stitchCamera(cameraIDs, 2, 1280, 720);
+	return 0;
 }
